eb696765051f0a8917479129d01a7afe02ae757a
sarukesh7119-boop <sarukesh7119@gmail.com>
2026-07-23 20:36:16 +0530

    Time: 2 ms (89.59%) | Memory: 42.4 MB (75.37%) - LeetSync

---
+++ 1195-distribute-candies-to-people/distribute-candies-to-people.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int ans[] = new int[num_people], i = 1, pos = 0;
+        while (candies > 0) {
+            ans[pos++] += candies >= i ? i : candies;
+            candies -= i;
+            i++;
+            pos %= num_people;
+        }
+        return ans;
+    }
+}
